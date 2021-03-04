edd035c14fae1125dc8fc8c5f4c8979470e73271
Othon Brück <othon.bruck@hotmail.com>
2021-03-04 01:16:19 -0300

    feat: Testando comandos da biblioteca discord.js

--- bot.js
+++ bot.js
@@ -1,10 +1,19 @@
 const Discord = require("discord.js");
+const guild = new Discord.Guild();
 const client = new Discord.Client();
+const api = require("./services/api");
+const endpoints = require("./services/endpoints");
 const config = require("./config.json");
 
-client.on("ready", () => {
-  console.log(client.channels);
-  client.user.setActivity(`Estou em ${client.guilds.size} servidores`);
+client.on("ready", async () => {
+  client.user.setActivity(`Em Manuntenção`);
+});
+
+client.on("message", (message) => {
+  if (message.author.bot) return;
+  if (message.content === "hello") {
+    message.reply(`hello ${message.channel.name}`);
+  }
 });
 
 client.login(config.token);
