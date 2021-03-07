4c9cacbe079a1410e974776543a8b6a205040588
Othon Brück <othon.bruck@hotmail.com>
2021-03-07 19:18:42 -0300

    feat: Criação de metódo para entrar em um canal e ouvir um arquivo mp3

--- bot.js
+++ bot.js
@@ -4,15 +4,33 @@ const client = new Discord.Client();
 const api = require("./services/api");
 const endpoints = require("./services/endpoints");
 const config = require("./config.json");
+const path = require("path");
 
 client.on("ready", async () => {
-  client.user.setActivity(`Em Manuntenção`);
+  client.user.setActivity(`Em Desenvolvimento`);
 });
 
-client.on("message", (message) => {
+client.on("message", async (message) => {
   if (message.author.bot) return;
-  if (message.content === "hello") {
-    message.reply(`hello ${message.channel.name}`);
+
+  if (message.content.startsWith(config.prefix)) {
+    const [CMD_NAME, ...args] = message.content
+      .trim()
+      .substring(config.prefix.length)
+      .split(/\s+/);
+    const { voice } = message.member;
+
+    console.log(CMD_NAME);
+
+    if (CMD_NAME === "COMANDO QUE DESEJA PARA ATIVAR O BOT") {
+      if (!voice.channelID) {
+        message.reply("Enter to a channel");
+      } else {
+        voice.channel.join().then((connection) => {
+          connection.play("LOCAL DO SEU ARQUIVO MP3", { volume: 0.5 });
+        });
+      }
+    }
   }
 });
 
